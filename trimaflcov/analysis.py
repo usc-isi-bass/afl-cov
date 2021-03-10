414e6c9be36e4198a843e9a0477a15b2885ac1dd
Wei-Cheng Wu <wwu@isi.edu>
2021-03-10 15:13:43 -0800

    Update

--- trimaflcov/analysis.py
+++ trimaflcov/analysis.py
@@ -15,10 +15,10 @@ class AflCoverage(Analysis):
             if target == "-":
                 return 
             t_name = target 
-            if self.project.loader.find_symbol(t_name) is None:
+            t_symbols = trim_analysis.find_func_symbols(self.project, t_name)
+            if len(t_symbols) == 0:
                 return
-            t_addr = self.project.loader.find_symbol(t_name).rebased_addr
-            self.target_addr = t_addr
+            self.target_addr = t_symbols[0].rebased_addr
         else:
             self.target_addr = target
 
@@ -31,10 +31,10 @@ class AflCoverage(Analysis):
     def _analyse(self):
         kb = self.project.kb
 
-        target_addr, pre_blocks, succ_blocks, trim_blocks = trim_analysis.get_target_pred_succ_trim_nodes(self.project, self.cfg, self.target_addr)
+        target_blocks, pre_blocks, succ_blocks, trim_blocks = trim_analysis.get_target_pred_succ_trim_nodes(self.project, self.cfg, [self.target_addr])
 
         # TODO
-        kb.cov.register_target_blocks(target_addr)
+        kb.cov.register_target_blocks(target_blocks)
         kb.cov.register_pre_blocks(pre_blocks)
         kb.cov.register_succ_blocks(succ_blocks)
         kb.cov.register_trim_blocks(trim_blocks)
